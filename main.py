528a97a98aa34930c0bdacf1c207865089e65d1b
ihor.shchuka <ihor.shchuka@gmail.com>
2023-10-10 17:35:16 +0300

    Added hardcoded default values for couple url

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 app = FastAPI()
 
 dict_urls = {
+    "google": "https://www.google.com",
+    "bbc": "https://www.bbc.com"
+
 }
 
 
 @app.post("/")
 async def root(long_url: Annotated[str, Form()]):
-    short_url = str(uuid.uuid4())
-    dict_urls[short_url] = long_url
+    if long_url.endswith(".ru"):
+        return "ЗАБЛОКОВАНО"
+    if long_url in dict_urls.values():
+        short_url = get_key_by_value(dict_urls, long_url)
+    else:
+        short_url = str(uuid.uuid4())
+        dict_urls[short_url] = long_url
     return short_url
 
 
@@ -31,9 +39,17 @@ async def index():
         </head>
         <body>
             <form action="/" method="post">
-                <input type="text" name="long_url" value="https://www.google.com" />
+                <label for="long_url">URL to add:</label><br>
+                <input type="text" id="long_url" name="long_url" value="https://www.google.com"><br>
                 <input type="submit" value="Go" />
             </form>
         </body>
     </html>
     """
+
+
+def get_key_by_value(dict_obj, value_to_find):
+    for key, value in dict_obj.items():
+        if value == value_to_find:
+            return key
+    return None
