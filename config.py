a210f1203b2a97a0b178b39122badd83828ad42a
ihor.shchuka <ihor.shchuka@gmail.com>
2023-10-13 16:35:47 +0300

    Added env variables

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from environment import get_environment
 
+MONGO_DB_SERVER = get_environment("MONGO_DB_SERVER")
 MONGO_INITDB_ROOT_USERNAME = get_environment("MONGO_INITDB_ROOT_USERNAME")
 MONGO_INITDB_ROOT_PASSWORD = get_environment("MONGO_INITDB_ROOT_PASSWORD")
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import motor.motor_asyncio
 from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 
-from config import MONGO_INITDB_ROOT_PASSWORD, MONGO_INITDB_ROOT_USERNAME
+from config import MONGO_DB_SERVER, MONGO_INITDB_ROOT_PASSWORD, MONGO_INITDB_ROOT_USERNAME
 
 app = FastAPI()
 mongo_client = motor.motor_asyncio.AsyncIOMotorClient(
-    f'mongodb://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@localhost:27017')
+    f'mongodb://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@{MONGO_DB_SERVER}:27017')
 db = mongo_client.deep_links
 
 
